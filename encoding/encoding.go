f69189168f19e7a7c8ddbd455d30e3b55bc1c285
qiuker521 <qiuker521@163.com>
2017-01-20 00:54:36 +0800

    add auto generate readme

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,22 +1,22 @@
 package utils
 
-// import (
-// 	"github.com/endeveit/enca"
-// )
+import (
+	"github.com/endeveit/enca"
+)
 
-// //EncodingTest is a third party library to get the encoding of certain type `[]byte`, which usually is UTF-8.
-// func EncodingTest(content *[]byte) (encoding string, err error) {
-// 	encoding = ""
-// 	defer func() {
-// 		if err := recover(); err != nil {
+//EncodingTest is a third party library to get the encoding of certain type `[]byte`, which usually is UTF-8.
+func EncodingTest(content *[]byte) (encoding string, err error) {
+	encoding = ""
+	defer func() {
+		if err := recover(); err != nil {
 
-// 		}
-// 	}()
+		}
+	}()
 
-// 	analyzer, err := enca.New("zh")
-// 	if err == nil {
-// 		encoding, err = analyzer.FromBytes(*content, enca.NAME_STYLE_ICONV)
-// 		defer analyzer.Free()
-// 	}
-// 	return
-// }
+	analyzer, err := enca.New("zh")
+	if err == nil {
+		encoding, err = analyzer.FromBytes(*content, enca.NAME_STYLE_ICONV)
+		defer analyzer.Free()
+	}
+	return
+}
