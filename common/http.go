24aa6584b9759bcbe7d5cda574e6c45cafa10d50
qiuker521 <qiuker521@163.com>
2017-04-23 00:59:34 +0800

    add auto generate readme

--- common/http.go
+++ common/http.go
@@ -4,7 +4,9 @@ import (
 	"crypto/tls"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
+	"strings"
 )
 
 //Get is a common http client for httpGet operations and hide UA as GoogleBot.
@@ -46,4 +48,53 @@ func getClient() *http.Client {
 		Timeout:   15 * time.Second,
 	}
 	return client
+}
+
+func RequestWithHeader(
+	method string,
+	url1 string,
+	timeout int,
+	header map[string]string,
+	params map[string]string,
+) ([]byte, error) {
+
+	client := getClient()
+
+	paramsA := ""
+	if method == "POST" {
+		values := url.Values{}
+		for k, v := range params {
+			values.Add(k, v)
+		}
+		paramsA = values.Encode()
+	}
+
+	req, _ := http.NewRequest(method, url1, strings.NewReader(paramsA))
+
+	for k, v := range header {
+		req.Header.Set(k, v)
+	}
+
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	if method == "GET" {
+		values := req.URL.Query()
+		for k, v := range params {
+			values.Add(k, v)
+		}
+		req.URL.RawQuery = values.Encode()
+	}
+
+	getResp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer getResp.Body.Close()
+	body, err := ioutil.ReadAll(getResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
 }
